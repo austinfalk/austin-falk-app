f4184982bb0edcb5d6e24bce74cd34e4f6972cb6
Austin Falk <austin@austinfalk.com>
2025-08-23 14:41:53 -0700

    fix(frontend): set up the route groups properly

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-import { LatestPost } from "@/app/_components/post";
+import { LatestPost } from "@/app/(frontend)/_components/post";
 import { HydrateClient, api } from "@/trpc/server";
 
 export default async function Home() {
